e54c771d020a000fc881a19b3867fb4822529c46
swethadevid <swetha96devi@gmail.com>
2019-01-22 09:56:37 +0530

    added countOccurenceOfCharacter program

--- JS-practisePrograms/CountOccurenceOFLetter.js
+++ JS-practisePrograms/CountOccurenceOFLetter.js
@@ -2,3 +2,18 @@
 //Sample arguments : 'gmail.com', 'm'
 //Expected output : 2
 
+
+function char_count(str, letter)
+{
+var letter_Count = 0;
+for (var position = 0; position < str.length; position++)
+{
+   if (str.charAt(position) == letter)
+     {
+     letter_Count += 1;
+     }
+ }
+ return letter_Count;
+}
+
+console.log("The occurence of 'o' is :",char_count('Facebook.com', 'o'));
